bbaee723b403b5276c8a167a8fe63a27f9661694
tr-nikolai <nikolacola20@gmail.com>
2017-09-13 22:45:37 +0300

    add bootstrap, css and new pages

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
     url(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
     url(r'^create/$', views.post_create),
-    url(r'^list/$', views.post_list, name='list'),
     url(r'^(?P<id>\d+)/delete/$', views.post_delete),
+    url(r'^$', views.post_list, name='list'),
 ]
--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import index
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index),
     url(r'^blog/', include('blog_posts.urls', namespace='blog')),
 ]
